dc0908a81895af795b2ba91baab2e0d95a0dca1a
Martinez <martinez@raidho.mx>
2015-09-16 18:56:18 -0500

    Work Single created, both template and sass, included into style.sass and header.php

--- templates/header.php
+++ templates/header.php
@@ -34,6 +34,7 @@ Neue Raidho Website
 				<li><a href="index.php">Home</a></li>
 				<li><a href="about.php">About</a></li>
 				<li><a href="work.php">Work</a></li>
+				<li><a href="work-single.php">Project</a></li>
 				<li><a href="services.php">Services</a></li>
 				<li><a href="recent.php">Recent</a></li>
 				<li><a href="contact.php">Contact</a></li>
---
+++ work-single.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Single Work Template
+Neue Raidho Website
+*/
+
+include "templates/header.php";
+?>
+
+<?php
+
+/*
+Extended Nav + Footer
+*/
+
+include "templates/extended_nav.php";
+include "templates/footer.php";
+
+?>
