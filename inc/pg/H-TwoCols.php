46eac43561b18f5209862b872aab89fb80a27603
raidho-mx <miranda@raidho.mx>
2015-11-05 10:56:28 -0600

    Two Cols & Members: Done

--- inc/pg/H-TwoCols.php
+++ inc/pg/H-TwoCols.php
@@ -1,12 +1,32 @@
 <div class="wrap">
 	<div class="Decima about_intro">
-		<div>
-			<img src="http://lorempixel.com/540/540/technics">
-			<h4>We work for a wide range of national and international clients, from Government Nanotechnology Clusters to Non-for-profit innovative organizations.</h4>
+		<div><?php
+			if(get_sub_field('choose')) {
+				$image = get_sub_field('img-1');
+				if($image) { ?>
+					<picture>
+						<img class="full_width_image"
+							 src="<?php echo $image['sizes']['larger']; ?>"
+							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
+							 alt="<?php echo $image['alt']; ?>" />
+					</picture><?php
+				}
+			} ?>
+			<h4><?php the_sub_field('txt-1'); ?></h4>
 		</div>
 		<div>
-			<h4>We also involve frequently with technology and design startups, getting to know them well and helping them improve the value of their products and services through rich media design and effective communication.</h4>
-			<img src="http://lorempixel.com/540/540/technics">
+			<h4><?php the_sub_field('txt-2'); ?></h4><?php
+			if(get_sub_field('choose')) {
+				$image = get_sub_field('img-2');
+				if($image) { ?>
+					<picture>
+						<img class="full_width_image"
+							 src="<?php echo $image['sizes']['larger']; ?>"
+							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
+							 alt="<?php echo $image['alt']; ?>" />
+					</picture><?php
+				}
+			} ?>
 		</div>
 	</div>
 </div>
--- inc/pg/I-Members.php
+++ inc/pg/I-Members.php
@@ -1,46 +1,57 @@
 <div class="wrap">
-	<h2>People at Raidho</h2>
+	<h2 class="Decima">People at Raidho</h2><?php
 
-	<div class="people">
-		<div class="founder">
-			<img src="http://lorempixel.com/600/600/technics">
-			<h4>David Quiroga</h4>
-			<p class="Decima">
-			Founder & Art Director<br>
-			<a href="mailto:quiroga@raidho.mx">quiroga@raidho.mx</a>
-			</p>
-		</div>
-		<div class="founder">
-			<img src="http://lorempixel.com/600/600/technics">
-			<h4>Francisco Martínez</h4>
-			<p class="Decima">
-			Founder & Lead Interaction Designer<br>
-			<a href="mailto:martinez@raidho.mx">martinez@raidho.mx</a>
-			</p>
-		</div>
-		<div>
-			<img src="http://lorempixel.com/600/600/technics">
-			<p>Miguel Balandrano</p>
-			<p class="Decima">
-			Designer<br>
-			<a href="mailto:acider@raidho.mx">acider@raidho.mx</a>
-			</p>
-		</div>
-		<div>
-			<img src="http://lorempixel.com/600/600/technics">
-			<p>Linda Castillo</p>
-			<p class="Decima">
-			Designer & Coder<br>
-			<a href="mailto:linda@raidho.mx">linda@raidho.mx</a>
-			</p>
-		</div>
-		<div>
-			<img src="http://lorempixel.com/600/600/technics">
-			<p>Rocko Raidho</p>
-			<p class="Decima">
-			PR Strategist<br>
-			<a href="mailto:rocko@raidho.mx">rocko@raidho.mx</a>
-			</p>
-		</div>
-	</div>
+
+	if(have_rows('founder')) : ?>
+	<div class="people_founders"><?php
+		while(have_rows('founder')) : the_row(); ?>
+			<div><?php
+				$image = get_sub_field('face');
+				if($image) { ?>
+					<picture>
+						<img class="full_width_image"
+							 src="<?php echo $image['sizes']['larger']; ?>"
+							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
+							 alt="<?php echo $image['alt']; ?>" />
+					</picture><?php
+				} ?>
+				<h4><?php the_sub_field('name'); ?></h4>
+				<p class="Decima">
+					<?php the_sub_field('position'); ?><br>
+					<a href="mailto:<?php the_sub_field('email'); ?>"><?php the_sub_field('email'); ?></a>
+				</p>
+			</div><?php
+
+		endwhile; ?>
+	</div><?php
+	endif;
+
+
+
+
+	if(have_rows('members')) : ?>
+	<div class="people_team"><?php
+		while(have_rows('members')) : the_row(); ?>
+			<div><?php
+				$image = get_sub_field('face');
+				if($image) { ?>
+					<picture>
+						<img class="full_width_image"
+							 src="<?php echo $image['sizes']['large']; ?>"
+							 <?php echo tevkori_get_srcset_string( $image['ID'], 'larger' ); ?>
+							 alt="<?php echo $image['alt']; ?>" />
+					</picture><?php
+				} ?>
+				<h4><?php the_sub_field('name'); ?></h4>
+				<p class="Decima">
+					<?php the_sub_field('position'); ?><br>
+					<a href="mailto:<?php the_sub_field('email'); ?>"><?php the_sub_field('email'); ?></a>
+				</p>
+			</div><?php
+
+		endwhile; ?>
+	</div><?php
+	endif; ?>
+
+
 </div>
