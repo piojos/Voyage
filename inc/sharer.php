cca20f8519ced782245038b118ba9668b9c8c389
raidho-mx <miranda@raidho.mx>
2015-11-11 18:45:50 -0600

    Blog Posts initial info

---
+++ inc/sharer.php
@@ -0,0 +1,5 @@
+<ul class="share_btns replica">
+	<li><a href="#">Post to Facebook</a></li>
+	<li><a href="#">Post to Twitter</a></li>
+	<li><a href="#">Copy URL</a></li>
+</ul>
--- inc/single-blog.php
+++ inc/single-blog.php
@@ -1,132 +1,32 @@
-<?php
+<div class="section_bottom_margins">
+	<img class="full_width_image" src="http://lorempixel.com/1280/740/technics">
+</div>
 
-/*
-Single Work Template
-Neue Raidho Website					:	50%
-Missing:
-- Slider template (Linda)
-*/
+<div class="wrap">
+	<div class="blog_wrap">
+		<div class="blog_header">
+			<h1 class="replica"><?php the_title(); ?></h1>
+			<div class="regular_title Leitura"><?php the_excerpt(); ?></div>
+			<p class="meta Decima">Posted on Blog</br> <?php the_time('j/m/Y'); ?></p><?php
 
-$bclass = "page";
-include "templates/header.php";
-?>
+			get_template_part('inc/sharer'); ?>
 
-    <?php /* LINDA: añadir funcionalidad de slider más adelante */ ?>
-	<div class="section_bottom_margins">
-		<img class="full_width_image" src="http://lorempixel.com/1280/740/technics">
+		</div>
+		<div class="blog_body Leitura">
+			<?php the_content(); ?>
+		</div>
+
+		<div class="blog_foot"><?php
+
+			get_template_part('inc/sharer'); ?>
+
+			<div class="blog_more">
+				<h3 class="Decima">More Blog Posts</h3>
+				<ul>
+					<li><img src="http://lorempixel.com/600/600/technics" alt=""><a href="#">BPR Monterrey, 4th Edition</a></li>
+					<li><img src="http://lorempixel.com/600/600/technics" alt=""><a href="#">Finishing that custom type, and how hackers might have some advice on the subject.</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
-
-    <div class="wrap">
-		<div>
-            <h1>Centro Médico ABC</h1>
-            <h2 class="Leitura">American British Cowdray</h2>
-
-            <h2 class="Leitura">After more than 25 years without a major brand redesign, we had the opportunity to completely revamp the brand and visual identity for this 130 years old hospital.</h2>
-        </div>
-    </div>
-
-    <?php // three pics one caption ?>
-    <div class="bl-party-three-one-caption">
-        <div><img src="http://lorempixel.com/640/750/technics"></div>
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-        <div>
-            <p>Centro de Cáncer ABC. Photos by: Blake Marvin, HKS Inc.</p>
-        </div>
-    </div>
-
-    <?php // titles ?>
-    <div class="wrap">
-        <div>
-            <p class="blue Decima">Centro Médico ABC</p>
-            <p class="Leitura medium_title">The ABC Hospital is ore than 120 years old, and it hadn’t undergo any major brand identity revamp since almost 20 years.</p>
-        </div>
-    </div>
-
-    <?php // three images with caption ?>
-    <div class="wrap bl-party-three-w-captions">
-        <ul>
-            <li>
-                <img src="http://lorempixel.com/380/300/technics">
-                <p class="small_paragraph Decima">1886 - Hospital Americano</p>
-            </li>
-            <li>
-                <img src="http://lorempixel.com/380/300/technics">
-                <p class="small_paragraph Decima">1923 - Hospital Inglés</p>
-            </li>
-            <li>
-                <img src="http://lorempixel.com/380/300/technics">
-                <p class="small_paragraph Decima">1941 - Centro Médico ABC American British Cowdray</p>
-            </li>
-        <ul>
-    </div>
-
-    <?php // two pics no caption ?>
-    <div class="bl-party-two-no-captions">
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-    </div>
-
-    <?php // three pics no caption ?>
-    <div class="bl-party-three-no-captions">
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-        <div><img src="http://lorempixel.com/640/374/technics"></div>
-    </div>
-
-    <?php // random grid ?>
-    <div class="wrap bl-party-random-grid section_pad">
-        <div class="rand-grid-a left">
-            <div class="rand-grid-a-top">
-                <img class="rand-grid-img-1" src="http://lorempixel.com/480/355/technics">
-            </div>
-            <div class="rand-grid-a-bottom">
-                <div>
-                    <img class="rand-grid-img-2" src="http://lorempixel.com/380/240/technics">
-                    <img class="rand-grid-img-3" src="http://lorempixel.com/280/260/technics">
-                </div>
-                <div>
-                    <img class="rand-grid-img-4" src="http://lorempixel.com/380/770/technics">
-                </div>
-            </div>
-        </div>
-        <div class="rand-grid-c right">
-            <img class="rand-grid-img-5" src="http://lorempixel.com/280/260/technics">
-            <img class="rand-grid-img-6" src="http://lorempixel.com/380/240/technics">
-            <img class="rand-grid-img-7" src="http://lorempixel.com/280/260/technics">
-            <img class="rand-grid-img-8" src="http://lorempixel.com/380/240/technics">
-        </div>
-    </div>
-
-<?php include "templates/contact_share.php"; ?>
-
-<?php // more projects ?>
-<section class="gray_light_bg">
-    <div class="wrap bl-party-three-w-captions">
-        <h2 class="Decima">Explore More Projects</h2>
-        <ul>
-            <li>
-                <img src="http://lorempixel.com/380/380/technics">
-                <p>Penguin Books. <span class="Leitura">An Expanding Universe Keynote.</span></p>
-            </li>
-            <li>
-                <img src="http://lorempixel.com/380/380/technics">
-                <p>Comunidad VIVEX. <span class="Leitura">Re-defining the News in Mexico.</span></p>
-            </li>
-            <li>
-                <img src="http://lorempixel.com/380/380/technics">
-                <p>Knock your Wear. <span class="Leitura">Professional Image Consulting.</span></p>
-            </li>
-        <ul>
-    </div>
-</section>
-
-<?php
-
-/*
-Extended Nav + Footer
-*/
-
-include "templates/footer.php";
-
-?>
+</div>
